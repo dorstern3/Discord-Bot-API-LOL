7100183f6f0c810552fd3b88561212931bc5c93c
dor <dorstern3@gmail.com>
2022-10-07 16:56:24 +0300

    90%

--- main.py
+++ main.py
@@ -48,12 +48,11 @@ async def stop(ctx):
  await ctx.send("**stop data**")
 
 
-
 # Play
 @bot.command()
 async def play(ctx):
   await ctx.send("**play data**")
-  data.start()
+  data.start(ctx)
  
   
   
@@ -73,21 +72,27 @@ async def play(ctx):
 #  await ctx.send(embed=embed)
 #  await asyncio.sleep(3)
 
-@tasks.loop(seconds=5)
-async def data():
+@tasks.loop()
+async def data(ctx):
+
  data = resp.json()
- guild = bot.get_guild(1014874558366502922)
- channel = bot.get_channel(1014874558366502925) # channel ID
- await channel.send('id: ' + str(data[0]['id']))
- await channel.send('name: ' + str(data[0]['localizedNames']['en_US']['name']))
-#  embed=discord.Embed(title = 'Data API',color = discord.Color.blue())
-#  embed.add_field(name='id', value = data[x]['id'], inline=False) # id
-#  embed.add_field(name='name', value = data[x]['localizedNames']['en_US']['name'], inline=False) # localizedNames -> name
-#  await ctx.send(embed=embed)
+
+ for x in range(325):
+  embed=discord.Embed(title = 'Data API for Player ' + str(x) ,color = discord.Color.red())
+  embed.add_field(name='id', value = data[x]['id'], inline=False) # id
+  embed.add_field(name='name', value = data[x]['localizedNames']['en_US']['name'], inline=False) # localizedNames -> name
+  embed.add_field(name='description', value = data[x]['localizedNames']['en_US']['shortDescription'], inline=False) # localizedNames -> description
+  embed.add_field(name='IRON', value = data[x]['thresholds']['IRON'], inline=False) # thresholds -> IRON
+  embed.add_field(name='GOLD', value = data[x]['thresholds']['GOLD'], inline=False) # thresholds -> GOLD
+  embed.add_field(name='SILVER', value = data[x]['thresholds']['SILVER'], inline=False) # thresholds -> SILVER
+  embed.add_field(name='DIAMOND', value = data[x]['thresholds']['DIAMOND'], inline=False) # thresholds -> DIAMOND
+  embed.add_field(name='BRONZE', value = data[x]['thresholds']['BRONZE'], inline=False) # thresholds -> BRONZE
+  embed.add_field(name='PLATINUM', value = data[x]['thresholds']['PLATINUM'], inline=False) # thresholds -> PLATINUM
+  embed.add_field(name='MASTER', value = data[x]['thresholds']['MASTER'], inline=False) # thresholds -> MASTER
+  await ctx.send(embed=embed)
+  await asyncio.sleep(3)
 
  
-  
-
 # Help -> Command Manual
 @bot.command(name='help')
 async def help(ctx):
@@ -101,5 +106,22 @@ async def help(ctx):
   embed.add_field(name='!stop', value='stop send data', inline=False)
   await ctx.send(embed=embed)
 
+
 bot.run(TOKEN)
 
+#  await try1(ctx)
+# @bot.event
+# async def try1(ctx):
+#   await ctx.send("try1")
+#   data = resp.json()
+#   guild = bot.get_guild(1014874558366502922)
+#   channel = bot.get_channel(1014874558366502925) # channel ID
+#   for x in range(10):
+#    await channel.send('id: ' + str(data[(x)]['id']))
+
+
+# await channel.send('id: ' + str(data[(x)]['id']))
+# await channel.send('name: ' + str(data[x]['localizedNames']['en_US']['name']))
+
+#    guild = bot.get_guild(1014874558366502922)
+#  channel = bot.get_channel(1014874558366502925) # channel ID
